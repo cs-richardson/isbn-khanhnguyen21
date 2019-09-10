cb8d6aa89210b44ce60569460d6991ce303712ca
Khanh Nguyen <khnguyen21@ssis.edu.vn>
2019-09-10 10:27:19 +0700

    Update ISBN.py

--- ISBN.py
+++ ISBN.py
@@ -1,16 +1,22 @@
 isbn = int(input('ISBN:'))
 total = 0
+#Taking the last number
 tenth = int(isbn % 10)
 isbn = int(isbn / 10)
+#This part is for finding the sum of the first 9 numbers
 for x in range(9):
     numIs = int(isbn % 10)
     multiply = numIs * (9-x)
     total = total + multiply
     isbn = isbn / 10
+#Finding the last number through the sum
 lastNum = int(total % 11)
+#Check if the last number found through the sum is equal to the last number
+#given by the input
 if lastNum == tenth:
     lastNum = lastNum * 10
     total = total + lastNum
+    #Checking for the legitimacy for the 2nd time
     if total % 11 == 0:
         print('Legit')
     else:
